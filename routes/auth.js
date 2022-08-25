0c496846d8aa529d3d904d6f66319e96e0cbb921
ClydeError <peterlishman18@gmail.com>
2022-08-25 13:57:38 +0300

    firs server commit 1.0v

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,16 @@
 import { Router } from "express"
+import { register, login, getME } from '../controllers/auth.js'
+import {checkAuth} from "../utils/checkAuth.js";
 
 const router = new Router()
 
 // Register
-
-router.post('/register', (req, res) => {} )
+router.post('/register', register)
 
 // Login
+router.post('/login', login)
 
 // Get me
+router.get('/me', checkAuth, getME)
 
 export default router
